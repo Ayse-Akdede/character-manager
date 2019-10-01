dbbd974db9f9d355a235ba764d09e42a3ab3e93a
LVannebenne <lindsay.vannebenne@gmail.com>
2019-10-01 14:30:49 +0200

    checking about css

--- index.ts
+++ index.ts
@@ -10,23 +10,14 @@ const url = "https://character-database.becode.xyz/";
 
 const tpl: HTMLElement = <HTMLElement>document.querySelector("#template");
 const target: HTMLElement = <HTMLElement>document.querySelector("#target");
+const tplSingle: HTMLElement = <HTMLElement>document.querySelector("#singleChar");
+const single: HTMLElement = <HTMLElement>document.querySelector("#single");
 
 let chars: Array<Object> = [];
 
 
 async function viewChar(id: String) {
     document.location.href = `viewchar?p=${id}`;
-    /* const single: HTMLElement = <HTMLElement>document.querySelector("#single");
-    axios.get(`${url}characters/${id}`).then(data => {
-        console.log(data.data);
-        let clone: HTMLElement = <HTMLElement>tpl.cloneNode(true).content;
-        clone.querySelector(".image").src = `data:image/jpeg;base64,${data.data.image}`;
-        clone.querySelector(".name").innerHTML = data.data.name;
-        clone.querySelector(".short-description").innerHTML = data.data.shortDescription;
-        clone.querySelector("#delete").onclick = function () { deleteChar(data.data.id) };
-        clone.querySelector("#modify").onclick = function () { editChar(data.data.id) };
-        single.appendChild(clone);
-    }).catch(err => { console.error(err) }) */
 }
 
 async function deleteChar(id: String) {
@@ -39,7 +30,7 @@ async function editChar(id: String) {
     await axios.get()
 }
 
-function displayCharacter(char: Object): void {
+function displayAllCharacter(char: Object): void {
 
     let clone: HTMLElement = <HTMLElement>tpl.cloneNode(true).content;
     clone.querySelector(".image").src = `data:image/png;base64,${char.image}`;
@@ -47,7 +38,7 @@ function displayCharacter(char: Object): void {
     clone.querySelector(".short-description").innerHTML = char.shortDescription;
     clone.querySelector("#delete").onclick = function () { deleteChar(char.id) };
     clone.querySelector("#modify").onclick = function () { editChar(char.id) };
-    clone.querySelector(".col-3").onclick = function () { viewChar(char.id) };
+    clone.querySelector(".wrapper").onclick = function () { viewChar(char.id) };
     target.appendChild(clone);
 }
 
@@ -57,21 +48,27 @@ async function getAllChar() {
             chars.push(elt);
         });
         chars.forEach((char, index) => {
-            displayCharacter(char);
+            displayAllCharacter(char);
         })
     }).catch(err => { console.error(err) })
 }
 
+async function getChar(id: String) {
+    axios.get(`${url}characters/${id}`).then(data => {
+        let clone: HTMLElement = <HTMLElement>tplSingle.cloneNode(true).content;
+        clone.querySelector(".image").src = `data:image/jpeg;base64,${data.data.image}`;
+        clone.querySelector(".name").innerHTML = data.data.name;
+        clone.querySelector(".description").innerHTML = data.data.Description;
+        clone.querySelector("#delete").onclick = function () { deleteChar(data.data.id) };
+        clone.querySelector("#modify").onclick = function () { editChar(data.data.id) };
+        single.appendChild(clone);
+    }).catch(err => { console.error(err) })
+}
 
 if (document.location.pathname == "/index") {
-    console.log(document.location);
     getAllChar();
-    console.log(tpl);
-    console.log(target);
 } else if (document.location.pathname.startsWith("/viewchar")) {
-    console.log(document.location);
-    console.log("hello");
-    console.log(document.location.search)
+    getChar(document.location.search.substring(3));
 } else {
     document.location.href = "/index";
 }
